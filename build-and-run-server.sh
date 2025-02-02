03063d180e7ff5f7dc274639dc2b2a890a57f0b1
Lock, Miguel <mlock002@student.franciscan.edu>
2025-02-02 17:47:18 -0500

    restructured file system, expanded build-and-run-server.sh

--- build-and-run-server.sh
+++ build-and-run-server.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-docker build -f ./express-docker/dockerfile.dev -t express-be/v0 ./express-docker
+#checks if node modules exist
+if [ ! -d "node_modules" ]; then
+    echo "Node modules not found. Installing..."
+  npm i express
+else
+    echo "Node modules found. Skipping installation."
+fi
+
+docker build -f dockerfile.dev -t express-be/v0 .
 
 docker run -d -it --rm -p 5007:5007 --name express-docker-dev express-be/v0
