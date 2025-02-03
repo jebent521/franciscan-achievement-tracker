b5cccc67df250eafd5a1d79f889c39385436a16c
Lock, Miguel <mlock002@student.franciscan.edu>
2025-02-03 16:04:11 -0500

    rest URLs for all & individual achievements now work with sample data

--- index.js
+++ index.js
@@ -7,5 +7,31 @@ app.use(express.json())
 app.get('/',
   (req, res) => res.send('Dockerizing Node Application'))
 
+app.get('/api/achievements', (req, res) => {
+  console.log('achievemets');
+
+  res.send(allAchievementsList);
+});
+
+app.get('/api/achievements/:id', (req, res) => {
+  const achievement = allAchievementsList.find(a => a.id === parseInt(req.params.id));
+  res.send(achievement || { error: 'Achievement not found', id: "404"});
+});
+
+class PossibleAchievements {
+  constructor(id, name, description, points) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.points = points;
+  }
+}
+
+const allAchievementsList = [
+  new PossibleAchievements(1, 'The Source and The Summit', 'Attend Mass on Campus', 10),
+  new PossibleAchievements(2, 'Cafarrhea', 'Eat at the Caf', 20),
+  new PossibleAchievements(3, 'Lets Shake Things Up', 'Eat at the Pub', 20)
+];
+
 app.listen(5007,
   () => console.log(`⚡[bootup]: Server is running at port: 5007`));
