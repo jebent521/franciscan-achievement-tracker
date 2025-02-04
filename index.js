bb799926a64e5e87c46039602c5805508fdcd248
MoonLight693 <whitmercraft939@gmail.com>
2025-02-04 15:10:52 -0500

    package jsons updated with yamljs to run with swagger yaml code and included path to api-docs page

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
-//const swaggerUi = require('swagger-ui-express');
-//const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
 const app = express();
 
-//const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// ... rest of your code
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
