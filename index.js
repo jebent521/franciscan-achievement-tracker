ce10ecf82a7d4f9549c6455cb5cebc0e09937da1
MoonLight693 <whitmercraft939@gmail.com>
2025-02-04 14:55:18 -0500

    swagger file set up for path achievements and achiements/id

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express'),
-app = express();
+const express = require('express');
+//const swaggerUi = require('swagger-ui-express');
+//const YAML = require('yamljs');
+const app = express();
+
+//const swaggerDocument = YAML.load('./swagger.yaml');
+
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
