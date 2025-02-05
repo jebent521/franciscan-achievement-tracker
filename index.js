b1957d1f6dcf01819cacc114e3bce9e63fbe0a9d
Lock, Miguel <mlock002@student.franciscan.edu>
2025-02-05 15:22:38 -0500

    Increased tab width from 2--> to match other files

--- index.js
+++ index.js
@@ -5,35 +5,35 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get('/',
-  (req, res) => res.send('Barons of Progress 📈 Homepage'))
+	(req, res) => res.send('Barons of Progress 📈 Homepage'))
 
 app.get('/api/achievements', (req, res) => {
-  console.log('achievemets');
+	console.log('achievemets');
 
-  res.send(allAchievementsList);
+	res.send(allAchievementsList);
 });
 
 app.get('/api/achievements/:id', (req, res) => {
-  const achievement = allAchievementsList.find(a => a.id === parseInt(req.params.id));
-  res.send(achievement || res.status(404).send('Achievement not found'));
+	const achievement = allAchievementsList.find(a => a.id === parseInt(req.params.id));
+	res.send(achievement || res.status(404).send('Achievement not found'));
 });
 
 class Achievements {
-  constructor(id, title, category, description, prerequisite, points) {
-    this.id = id;
-    this.title = title;
-    this.category = category;
-    this.description = description;
-    this.prerequisite = prerequisite;
-    this.points = points;
-  }
+	constructor(id, title, category, description, prerequisite, points) {
+		this.id = id;
+		this.title = title;
+		this.category = category;
+		this.description = description;
+		this.prerequisite = prerequisite;
+		this.points = points;
+	}
 }
 
 const allAchievementsList = [
-  new Achievements(1, 'The Source and The Summit', "Franny Basics", 'Attend Mass on Campus', null, 10),
-  new Achievements(2, 'Cafarrhea', "Franny Basics", 'Eat at the Caf', null, 20),
-  new Achievements(3, 'Lets Shake Things Up', "Franny Basics", 'Eat at the Pub', null, 20)
+	new Achievements(1, 'The Source and The Summit', "Franny Basics", 'Attend Mass on Campus', null, 10),
+	new Achievements(2, 'Cafarrhea', "Franny Basics", 'Eat at the Caf', null, 20),
+	new Achievements(3, 'Lets Shake Things Up', "Franny Basics", 'Eat at the Pub', null, 20)
 ];
 
 app.listen(5007,
-  () => console.log(`⚡[bootup]: Server is running at port: 5007`));
+	() => console.log(`⚡[bootup]: Server is running at port: 5007`));
