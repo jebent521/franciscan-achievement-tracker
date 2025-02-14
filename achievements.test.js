b4c8f4aa084e53751c2c304d276a8c1b8cddda8a
ewhitmer001 <ewhitmer001@student.franciscan.edu>
2025-02-14 17:04:36 -0500

    test for default values of achievements class

--- achievements.test.js
+++ achievements.test.js
@@ -1,8 +1,34 @@
 // How to include a class!
 const achievements = require('./achievements');
 
-test ("Test achievements class", () => {
-    const achievement = new achievements(1);
+const achievement = new achievements();
+
+test ("Test achievements class default values: id", () => {
     //toBeInstanceOf
-    expect(achievement.id).toBe(1);
+    expect(achievement.id).toBe(-1);
+})
+
+test ("Test achievements class default values: title", () => {
+    //toBeInstanceOf
+    expect(achievement.title).toBe("Empty Title");
+})
+
+test ("Test achievements class default values: category", () => {
+    //toBeInstanceOf
+    expect(achievement.category).toBe("No category");
+})
+
+test ("Test achievements class default values: description", () => {
+    //toBeInstanceOf
+    expect(achievement.description).toBe("Empty description");
+})
+
+test ("Test achievements class default values: prerequisite", () => {
+    //toBeInstanceOf
+    expect(achievement.prerequisite).toBe(null);
+})
+
+test ("Test achievements class default values: points", () => {
+    //toBeInstanceOf
+    expect(achievement.points).toBe(10);
 })
