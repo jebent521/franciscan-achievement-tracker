bb815405d1d9502579a9b7c65974232d39b7d8de
ewhitmer001 <ewhitmer001@student.franciscan.edu>
2025-02-15 20:41:56 -0500

    moved tests into unit test folder

--- testing/unit_testing/achievement.test.js
+++ testing/unit_testing/achievement.test.js
@@ -1,7 +1,7 @@
 // How to include a class!
-const Achievement = require('../src/models/achievement');
+const Achievement = require('../../src/models/achievement');
 
-test ("Test achievements class: overloading equals()", () => {
+test("Test achievements class: overloading equals()", () => {
     const achievement1 = new Achievement();
     const achievement2 = new Achievement();
     expect(achievement1.equals(achievement2)).toBe(true);
---
+++ testing/unit_testing/user.test.js
@@ -0,0 +1,26 @@
+const User = require('../../src/models/user');
+
+// Good data test
+const user1 = new User("bobert24", "1234", "bob@gmail.com"); // the original
+const user2 = new User("bobert24", "1234", "bob@gmail.com"); // the copy
+
+test("Test user class: see if users are copies of each other.", () => {
+    expect(user1.equals(user2)).toBe(true);
+})
+
+// Bad data Test
+const user3 = new User("Heartseeker", "1234", "bob@gmail.com"); // one difference
+const user4 = new User("Heartseeker", "2121", "bob@gmail.com"); // two differences
+const user5 = new User("Heartseeker", "2121", "Amy@gmail.com"); // three differences
+
+test("Test user class: see if users are copies of each other.", () => {
+    expect(user1.equals(user3)).toBe(false);
+})
+
+test("Test user class: see if users are copies of each other.", () => {
+    expect(user1.equals(user4)).toBe(false);
+})
+
+test("Test user class: see if users are copies of each other.", () => {
+    expect(user1.equals(user5)).toBe(false);
+})
