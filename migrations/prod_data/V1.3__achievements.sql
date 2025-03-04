b6b46299e040540862fb7031026f227400031398
Jonah Ebent <jebent001@student.franciscan.edu>
2025-03-04 18:56:31 -0500

    separate dev and prod data

---
+++ migrations/prod_data/V1.3__achievements.sql
@@ -0,0 +1,5 @@
+INSERT INTO achievements (title, group_id, description, points)
+VALUES
+    ('Cafarrhea', 1, 'Eat at the Caf', 10),
+    ('The Grand Slam', 2, 'Attend all four daily masses in one day', 100),
+    ('Four Years of B.S.', 3, 'Be a STEM major', 20);
