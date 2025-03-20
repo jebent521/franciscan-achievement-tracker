b6decff6808ed5a13025c20d2629de8da468036e
Jonah Ebent <jebent001@student.franciscan.edu>
2025-03-20 13:01:41 -0400

    arg-parsing

--- src/services/search-service.js
+++ src/services/search-service.js
@@ -2,7 +2,36 @@ const ApiResult = require('../utils/api-result');
 
 class SearchService {
   async search(req, res) {
-    res.status(501).send();
+    const query = req.params.query;
+
+    var users = false;
+    var groups = false;
+    var achievements = false;
+
+    var filter;
+    if (!req.query.filter) filter = ['users', 'groups', 'achievements'];
+    else filter = req.query.filter.split(',');
+    for (const f of filter) {
+      switch (f) {
+        case 'users':
+          users = true;
+          break;
+        case 'groups':
+          groups = true;
+          break;
+        case 'achievements':
+          achievements = true;
+          break;
+        default:
+          res.status(400).send(`Invalid filter parameter '${f}'`);
+          return;
+      }
+    }
+    res.send(`Query: ${query}
+Filter:
+  users: ${users}
+  groups: ${groups}
+  achievements: ${achievements}`);
   }
 }
 
