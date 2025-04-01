5e1ad9cf6d5301019d3533f463cd64c641369eb7
Jonah Ebent <jebent001@student.franciscan.edu>
2025-04-01 11:10:31 -0400

    used routers to cleanup index.js

---
+++ src/routes/achievements.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const AchievementService = require('../services/achievement-service');
+
+router.get('/', async (_, res) => {
+  const result = await new AchievementService().read();
+  res.status(result.status).send(result.message);
+});
+
+router.get('/:id', async (req, res) => {
+  const result = await new AchievementService().readById(req);
+  res.status(result.status).send(result.message);
+});
+
+router.post('/', async (req, res) => {
+  const result = await new AchievementService().create(req);
+  res.status(result.status).send(result.message);
+});
+
+router.put('/:id', async (req, res) => {
+  const result = await new AchievementService().update(req);
+  res.status(result.status).send(result.message);
+});
+
+router.delete('/:id', async (req, res) => {
+  const result = await new AchievementService().delete(req);
+  res.status(result.status).send(result.message);
+});
+
+module.exports = router;
